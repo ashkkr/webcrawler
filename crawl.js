ab45d19fc916c7b24f240b80e2b37b8fa05d2882
ashutosh <ashutoshsangra@gmail.com>
2023-06-17 08:14:11 +0530

    wrote test cases

--- crawl.js
+++ crawl.js
@@ -3,13 +3,29 @@ const { JSDOM } = require('jsdom')
 function getUrlFromHtml(htmlBody, baseUrl) {
     const urls = []
     const dom = new JSDOM(htmlBody)
-    console.log(htmlBody)
-    const linkelements = dom.window.document.querySelectorAll('a')
-    console.log(linkelements)
+    const linkelements = dom.window.document.getElementsByTagName('a');
 
-    // for (const linkelement in linkelements) {
-    //     console.log(linkelement)
-    // }
+    for (let i = 0; i < linkelements.length; i++) {
+        if (linkelements[i].getAttribute("href").slice(0, 1) == '/') {
+            try {
+                const urltemp = new URL(baseUrl + linkelements[i].getAttribute("href"))
+                urls.push(baseUrl + linkelements[i].getAttribute("href"))
+            }
+            catch (err) {
+                console.log("error with absolute url " + err.message)
+            }
+        }
+        else {
+            try {
+                const urltemp = new URL(linkelements[i].getAttribute("href"))
+                urls.push(linkelements[i].getAttribute("href"))
+            }
+            catch (err) {
+                console.log("error with absolute url " + err.message)
+            }
+
+        }
+    }
 
     return urls
 }
--- crawl.test.js
+++ crawl.test.js
@@ -24,10 +24,34 @@ test("basicCrawler capitals", () => {
 })
 
 test("getUrlsFromHtml", () => {
-    const inputHTML = `<html><body><a href="https://blog.boot.dev/">boot dev blog</a></body></html>`
+    const inputHTML = `<html><body><a href="https://blog.boot.dev/">boot dev blog</a>
+    </body></html>`
     const inputUrl = "https://blog.boot.dev"
     const expected = ["https://blog.boot.dev/"]
     const actual = getUrlFromHtml(inputHTML, inputUrl)
     expect(actual).toEqual(expected)
 })
 
+test("getUrlsFromHtml relative", () => {
+    const inputHTML = `<html>
+    <body>
+    <a href="/path/">boot dev blog</a>
+    </body>
+    </html>`
+    const inputUrl = "https://blog.boot.dev"
+    const expected = ["https://blog.boot.dev/path/"]
+    const actual = getUrlFromHtml(inputHTML, inputUrl)
+    expect(actual).toEqual(expected)
+})
+
+test("getUrlsFromHtml invalid", () => {
+    const inputHTML = `<html>
+    <body>
+    <a href="://432.44/4">boot dev blog</a>
+    </body>
+    </html>`
+    const inputUrl = "https://blog.boot.dev"
+    const expected = []
+    const actual = getUrlFromHtml(inputHTML, inputUrl)
+    expect(actual).toEqual(expected)
+})
